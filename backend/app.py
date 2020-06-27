2c88801a144ca1def6c8729f1cf41389271daa84
marcusee <helmetpotato@hotmail.com>
2020-06-27 19:06:13 +0800

    post functions

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/generate-report' ,  methods = ['POST'])
+def generate_report():
+    req_data = request.get_json()
+    print(req_data["sar"])
+    
+    return req_data;
 
 if __name__ == "__main__":
     app.run(debug=True)
